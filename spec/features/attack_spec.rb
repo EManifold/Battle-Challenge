a55962a60f722bd591367d24ea65458cfb1dceaa
Julie <j49411@gmail.com>
2019-11-28 16:58:55 +0000

    add attack and receive damage

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,10 @@ feature "attack" do
     click_link "Attack"
     expect(page).to have_content "Julie attacked Harrison"
   end
+
+  scenario "reduce player 2's hit points by 10" do
+    sign_in_and_play
+    click_link "Attack"
+    expect(page).to have_content "Harrison: 50HP"
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,31 @@ require 'player'
 
 describe Player do
   subject(:julie) { Player.new('Julie') }
+  subject(:harrison) { Player.new('Harrison') }
 
   describe '#name' do
     it 'should return the player name' do
       expect(julie.name).to eq 'Julie'
     end
   end
+
+  describe '#hit_points' do
+    it 'should return hit points' do
+      expect(julie.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(harrison).to receive(:receive_damage)
+      julie.attack(harrison)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'should reduce the player hit points' do
+      expect{ harrison.receive_damage }.to change { harrison.hit_points }.by -10
+    end
+  end
+
 end
