5fdb88c16a76da4777b6feddc22258a994e831a7
Ellie <elliemmanifold@gmail.com>
2019-11-28 14:26:54 +0000

    Create web_helper file

--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -1,9 +1,6 @@
 feature "enter the players" do
     scenario "fill in player forms" do
-        visit '/'
-        fill_in :player_1, with: "Julie"
-        fill_in :player_2, with: "Harrison"
-        click_button 'Submit'
-        expect(page).to have_content "Julie vs Harrison"
+      sign_in_and_play
+      expect(page).to have_content "Julie vs Harrison"
     end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "display hit points" do
   scenario "show player 2's hit points" do
-    visit '/'
-    fill_in :player_1, with: "Julie"
-    fill_in :player_2, with: "Harrison"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Harrison: 60HP"
-  end 
+  end
 end
