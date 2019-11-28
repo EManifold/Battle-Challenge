79bfe0526389aadb32d57841d02e878fba7efa53
Julie <j49411@gmail.com>
2019-11-28 14:03:24 +0000

    edit session

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    sessions[:player_1] = params[:player_1]
-    sessions[:player_2] = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = sessions[:player_1]
-    @player_2 = sessions[:player_2]
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
